1f1e1d5b05503e4f6e4c9c021bc6756947a48928
huytq@ics-p.vn <huytq@ics-p.vn>
2024-10-15 08:51:43 +0700

    add extension

---
+++ src/AppCoreAPI/Extensions/ClaimsPrincipleExtensions.cs
@@ -0,0 +1,11 @@
+﻿using System.Security.Claims;
+
+namespace AppCoreAPI.Extensions
+{
+    public static class ClaimsPrincipleExtensions
+    {
+        public static string GetUserName(this ClaimsPrincipal user) { return user.FindFirst(ClaimTypes.Name)?.Value; }
+
+        public static int GetUserId(this ClaimsPrincipal user) { return int.Parse(user.FindFirst(ClaimTypes.NameIdentifier)?.Value); }
+    }
+}
