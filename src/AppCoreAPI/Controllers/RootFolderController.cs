6525c0e8fe7c9d36f6ee4ef80e6db07476913c65
huytq@ics-p.vn <huytq@ics-p.vn>
2024-10-15 09:06:02 +0700

    Add API RootFolder và Shared

---
+++ src/AppCoreAPI/Controllers/RootFolderController.cs
@@ -0,0 +1,51 @@
+﻿using AppCoreAPI.Data.Entities;
+using AppCoreAPI.Dtos;
+using AppCoreAPI.Errors;
+using AppCoreAPI.Extensions;
+using AppCoreAPI.SeedWorks.Interfaces;
+using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AppCoreAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RootFolderController : ControllerBase
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IMapper _mapper;
+
+        public RootFolderController(IUnitOfWork unitOfWork, IMapper mapper)
+        {
+            _mapper = mapper;
+            _unitOfWork = unitOfWork;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> PostNewRoot()
+        {
+            var rootFolder = new RootFolderDto
+            {
+                Name = Guid.NewGuid().ToString().Substring(0, 10),
+                UserId = User.GetUserId(),
+            };
+
+            var rootFolderDb = _mapper.Map<RootFolderDto, RootFolder>(rootFolder);
+            _unitOfWork.RootFolderRepository.Add(rootFolderDb);
+
+            if(await _unitOfWork.Complete())
+                return Ok(rootFolder);
+
+            return BadRequest(new ApiResponse(400, "Can not add"));
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<RootFolder>> GetRootFoler(int id)
+        {
+            var rootFolder = await _unitOfWork.RootFolderRepository.GetRootFolder(id);
+
+            return Ok(rootFolder);
+        }
+    }
+}
---
+++ src/AppCoreAPI/Controllers/SharedController.cs
@@ -0,0 +1,89 @@
+﻿using AppCoreAPI.Data.Entities;
+using AppCoreAPI.Dtos;
+using AppCoreAPI.Errors;
+using AppCoreAPI.Extensions;
+using AppCoreAPI.Helpers;
+using AppCoreAPI.SeedWorks.Interfaces;
+using AppCoreAPI.Services.Interfaces;
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AppCoreAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SharedController : ControllerBase
+    {
+        private readonly IGoogleDriveService _googleDriveService;
+        private readonly IMapper _mapper;
+        private readonly IUnitOfWork _unitOfWork;
+
+        public SharedController(IGoogleDriveService googleDriveService, IUnitOfWork unitOfWork, IMapper mapper)
+        {
+            _unitOfWork = unitOfWork;
+            _googleDriveService = googleDriveService;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> GetShortUrl([FromQuery] FileOrFolderParams fileOrFolderParams)
+        {
+            var entity = await _unitOfWork.SharedToUserRepository.GetSharedToUserByUrl(fileOrFolderParams.Url, User?.Identity?.Name!);
+            if (entity == null)
+                return Unauthorized(new ApiResponse(401, "You are Unauthorized"));
+
+            fileOrFolderParams.Path = entity.FullPath;
+
+            var list = await _googleDriveService.GetFileAndFolders(fileOrFolderParams);
+
+            Response.AddPaginationHeader(list.CurrentPage, list.PageSize, list.TotalCount, list.TotalPages);
+
+            return Ok(new
+            {
+                data = list,
+                parentPath = entity.FullPath
+            });
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> PostShortUrl(SharedToUserAddDto sharedToUser)
+        {
+            sharedToUser.Url = Guid.NewGuid().ToString().Substring(0, 8);
+            sharedToUser.OwnerUserName = User?.Identity?.Name!;
+            var sharedToUserEntity = _mapper.Map<SharedToUserAddDto, SharedToUser>(sharedToUser);
+            if (sharedToUser.SharedUserName != null || sharedToUser?.SharedUserName?.Length != 0)
+            {
+                foreach (var username in sharedToUser?.SharedUserName!)
+                {
+                    // khong them chinh no
+                    if (User?.Identity?.Name != username)
+                    {
+                        var entity = new SharedToUser
+                        {
+                            FullPath = sharedToUserEntity.FullPath,
+                            IsFolder = sharedToUserEntity.IsFolder,
+                            Name = sharedToUserEntity.Name,
+                            OwnerUserName = sharedToUserEntity.OwnerUserName,
+                            SharedUserName = username,
+                            ShortUrl = sharedToUserEntity.ShortUrl,
+                            Url = sharedToUserEntity.Url
+                        };
+                        _unitOfWork.SharedToUserRepository.Add(entity);
+                    }
+                }
+                if (await _unitOfWork.Complete())
+                    return Ok(new
+                    {
+                        url = sharedToUser.Url,
+                        parentPath = sharedToUserEntity.FullPath//khong su dung
+                    });
+                else
+                    return BadRequest(new ApiResponse(400, "Error while add entity to shared database"));
+            }
+            else
+            {
+                return NoContent();
+            }
+        }
+    }
+}
